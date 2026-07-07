ef438d40ed03863fb78ebdebe68f989df8ce5c31
Lux-aurorae <sherlock216@naver.com>
2026-07-07 22:36:02 +0900

    Modify-Gate

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 let resetButton = document.getElementById("reset-button");
+let chances = 3;
+// 기회는 3번으로 제한 (과제제출시)
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
@@ -24,6 +28,16 @@ function pickRandomNum() {
 
 function play() {
     let userValue = userInput.value;
+
+    chances -- ;
+    chanceArea.textContent = `남은 찬스: ${chances}번`;
+    // 숫자 변수값만 넣고 싶을 때, '남은 찬스: chances 만 넣으면 안된다.
+    // 정적인 값과 동적인 값을 같이 넣어주고 싶다면 중요! 약간 다른 문법을 사용해야해야한다.
+    //  "" 의 값은 정적인 값에 ''(백틱) 의 값은 동적인 값에 사용한다.
+    // 
+    console.log("chances", chances);
+    // chances = chances - 1; // chances -= 1; // chances--; 동일한 의미
+
     if (userValue < computerNum) {
         resultArea.textContent = "up!!!";
     } else if (userValue > computerNum) {
@@ -31,12 +45,26 @@ function play() {
     } else {
         resultArea.textContent = "맞췄습니다!";
     }
+
+    if (chances < 1){
+        gameOver = true;
+    }
+    // 긁적긁적 빛나 강사님도 실수한 부분! 왜 실수 했을까? 
+    // let 변수를 선언하고 chances 라는 고유 ID 값에 = 3 3번 기회로 설정하였고,
+    // 3번 설정 값에 1 밑으로 떨어지면( < 1) 버튼이 정지되어야 하는데 < 3 이라고 하면 3 이하부터는 작동하지 않는 문제가 발생함
+
+if (gameOver == true) {
+    playButton.disabled = true;
 }
+}
+
 function reset(){
     // user input 창이 깨끗하게 정리되고
     userInput.value = "";
     // 새로운 번호가 생성되고
     pickRandomNum();
+
+    resultArea.textContent = "결과값이 나옵니다";
 }
     pickRandomNum();
 
